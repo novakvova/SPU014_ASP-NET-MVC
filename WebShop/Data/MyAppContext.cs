2f94f4c757f7b9c0bc3759f231bc824c3ce7bad5
novakvova <novakvova@gmail.com>
2022-09-10 11:28:06 +0300

    Begin add Context and Category table

---
+++ WebShop/Data/MyAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Data.Entities;
+
+namespace WebShop.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options)
+            : base(options) { }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+    }
+}
